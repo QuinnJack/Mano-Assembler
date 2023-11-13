9733dd70fee0f7b4dd12d599e839307a12e2431f
QuinnJack <108008843+QuinnJack@users.noreply.github.com>
2023-11-12 22:50:57 -0500

    Add react app

---
+++ theme.config.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+  },
+
+  footer: {
+  },
+}
+
+export default config
