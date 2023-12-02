88606ad62ec69f6ec0f052c6da95eec71124507a
QuinnJack <108008843+QuinnJack@users.noreply.github.com>
2023-12-02 17:44:31 -0500

    Update theme.config.tsx to remove header and footer

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,11 +3,8 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Mano Machine</span>,
-  project: {
-  },
 
-  footer: {
-  },
+
 }
 
 export default config
